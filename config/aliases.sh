6bae71958eb692b06c1794f7b3de93bb1937ed6f
Braulio <barahona.braulio@gmail.com>
2020-01-08 11:54:19 +0000

    aliases finished

--- config/aliases.sh
+++ config/aliases.sh
@@ -1,18 +1,21 @@
 # ----------------------
 # Git Command Aliases
 # ----------------------
+gco(){ git checkout "$1";}
+alias gst='git status'		
 alias ga='git add .'
 alias glg='git log --pretty=format:%C(yellow)%h%Cred%d\\ %Creset%s%Cgreen\\ [%ae,%ar]” --decorate --graph'
 alias gpr='git pull --rebase'
-alias guncommit='git reset --soft HEAD~1'
-alias gco='git checkout'
-alias gcob='git checkout -b'
-alias gfilelist='git diff-tree --no-commit-id --name-only -r'
-alias gdelb='git branch -D'
-alias gst='git status'
-alias gcmm='git log --format=%B -n 1'
-alias gamend='git commit --amend --no-edit'
-alias gamende='git commit --amend'
-alias grecommit='git commit -c ORIG_HEAD'
-alias grenameb='git branch -m'
-alias gstashu='git stash -k -u'
+alias gfiles='git diff-tree --no-commit-id --name-only -r'	# Show files changed in last commit
+alias gstashu='git stash -k -u'								# Stash all (even newly created files)
+# Commit related aliases
+gnc(){ git commit -m "$1";}									# Creates new commit
+alias gcm='git log --format=%B -n 1' 						# Show last commit message
+alias gamend='git commit --amend --no-edit' 				# Amend
+alias gamende='git commit --amend'							# Amend and edit
+alias guncommit='git reset --soft HEAD~1'					# Destroys last commit
+alias grecommit='git commit -c ORIG_HEAD'					# Recommit
+# Branch related aliases
+gcob(){ git checkout -b "$1";}								# Checkout to new branch
+grenameb(){ git branch -m "$1";}							# Rename active branch
+gdelb(){ git branch -D "$1";}								# Delete branch
--- utils/set_aliases.sh
+++ utils/set_aliases.sh
@@ -1,56 +1,31 @@
 function join_by { local IFS="$1"; shift; echo "$*"; }
-function explode_by () {
-	echo "first parameter is $1"
-	echo "second parameter is $2"
-
-	local IFS=$1; read -r -a EXPLODED <<< $2
-	echo "here"
-	echo ${EXPLODED[@]}
-	echo "$EXPLODED"
-}
-
-
+function explode_by () { local arr=($(echo $2 | tr "$1" "\n")); echo ${arr[@]};}
 
 HOME_DIR="$(eval echo ~$USER)"
 FILE_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+EXPLODED_PATH=($(explode_by / ${FILE_DIR})) # Parenthesis transform returned value into an array
 
-test=$(explode_by / ${FILE_DIR})
-
-echo "my test is ${test}"
-
-
-IFS='/' # Delimeter
-read -ra EXPLODED_PATH <<< "$FILE_DIR" # Explode string into array
 unset "EXPLODED_PATH[${#EXPLODED_PATH[@]}-1]" # Remove last element from array
+ALIASES_CONFIG_PATH="/$(join_by / ${EXPLODED_PATH[@]} config aliases.sh)"
 
-ALIASES_CONFIG_PATH=$(join_by / ${EXPLODED_PATH[@]} config aliases.sh)
+FILENAME=".bashrc"
+FILE="${HOME_DIR}/${FILENAME}"
 
+if ! test -f "$FILE"; then
+	echo "${FILENAME} does not exist, creating it now"
+	$(cd $HOME_DIR && touch $FILENAME)
+	echo "${FILENAME} created"
+fi
 
+if ! test -f "$ALIASES_CONFIG_PATH"; then
+	echo "${ALIASES_CONFIG_PATH} doesn't exist"
+	exit
+fi
 
-# echo "my path is ${ALIASES_CONFIG_PATH}"
+echo "" >> "$FILE"
+echo "" >> "$FILE"
+cat $ALIASES_CONFIG_PATH >> $FILE
 
+echo "All aliases have been saved in ${FILENAME}"
 
-
-# ALIASES=../config/aliases.sh
-
-
-
-
-
-
-# FILENAME=".testrc"
-
-
-# FILE="${HOME_DIR}/${FILENAME}"
-
-# if test -f "$FILE"; then
-# 	echo "exists"
-# else
-# 	echo "${FILENAME} file is not present, creating it now"
-# 	$(cd $HOME_DIR && touch $FILENAME)
-# 	echo "${FILENAME} created"
-# fi
-
-
-# echo $parent_path
-# echo "$ALIASES" >> "$FILE"
+source $FILE
